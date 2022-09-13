8d67d4141b240ccbd858de12dcb6d58d9d138736
Emil <50961259+EmK530@users.noreply.github.com>
2022-09-13 22:33:14 +0200

    Change version string to v2.1.3

--- StartupHandler.cs
+++ StartupHandler.cs
@@ -2,7 +2,7 @@ namespace SharpMIDI
 {
     class UserInput
     {
-        private static string version = "v2.1.2";
+        private static string version = "v2.1.3";
         static void Main(string[] args)
         {
             #if DEBUG
