baf8729b2e8f1e34c217fd5e68cacdc176a83942
Emil <50961259+EmK530@users.noreply.github.com>
2022-09-26 09:55:25 +0200

    Version edit

--- StartupHandler.cs
+++ StartupHandler.cs
@@ -2,7 +2,7 @@ namespace SharpMIDI
 {
     class UserInput
     {
-        private static string version = "v2.2.1";
+        private static string version = "v2.3.0";
         static void Main(string[] args)
         {
             #if DEBUG
