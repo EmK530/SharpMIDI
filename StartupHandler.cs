a70a8f6c8452a6975b98810c063d0fe0abb5070c
Emil <50961259+EmK530@users.noreply.github.com>
2022-09-27 14:26:27 +0200

    New buffer option

--- StartupHandler.cs
+++ StartupHandler.cs
@@ -2,7 +2,7 @@ namespace SharpMIDI
 {
     class UserInput
     {
-        private static string version = "v2.3.1";
+        private static string version = "v2.3.2";
         static void Main(string[] args)
         {
             #if DEBUG
@@ -19,7 +19,7 @@ namespace SharpMIDI
             }
             string? PathInput = null;
             while(true){
-                Console.Write("Enter MIDI Path: ");
+                Console.Write("Setting #1 / #3 | Enter MIDI Path: ");
                 PathInput = Console.ReadLine();
                 if(PathInput == null){Console.WriteLine("\nInvalid input of null");}else{
                     PathInput=PathInput.Replace("& '".ToString(), String.Empty);
@@ -29,7 +29,7 @@ namespace SharpMIDI
             }
             byte Threshold = 0;
             while(true){
-                Console.Write("Pick note threshold (0-127): ");
+                Console.Write("Setting #2 / #3 | Pick note threshold (0-127): ");
                 string? temp = Console.ReadLine();
                 if(temp == null){Console.WriteLine("\nInvalid input of null");}else{
                     if (int.TryParse(temp, System.Globalization.NumberStyles.HexNumber, null, out int i))
@@ -41,7 +41,27 @@ namespace SharpMIDI
                     }
                 }
             }
-            MIDIReader.LoadPath(PathInput,Threshold);
+            int maxBuffer = 0;
+            while(true){
+                Console.Write("Setting #3 / #3 | Max track buffer (limit 2147483647): ");
+                string? temp = Console.ReadLine();
+                if(temp == null){Console.WriteLine("\nInvalid input of null");}else{
+                    if (long.TryParse(temp, System.Globalization.NumberStyles.HexNumber, null, out long i))
+                    {
+                        if(i < 8192){
+                            Console.WriteLine("\nBuffer size below 8192 is not recommended.");
+                        } else if(i > 2147483647){
+                            Console.WriteLine("\nBuffer size over 2147483647 unsupported");
+                        } else {
+                            maxBuffer = (int)i;
+                            break;
+                        }
+                    } else {
+                        Console.WriteLine("\nCould not parse input");
+                    }
+                }
+            }
+            MIDIReader.LoadPath(PathInput,Threshold,maxBuffer);
         }
     }
 }
