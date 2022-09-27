63598e5eba1386b724040384642e5601b9f6efa8
Emil <50961259+EmK530@users.noreply.github.com>
2022-09-27 20:11:53 +0200

    Command line support for v2.4.0

--- WinMM.cs
+++ WinMM.cs
@@ -31,44 +31,53 @@ namespace SharpMIDI
         [DllImport("winmm.dll")]
         public static extern uint midiOutShortMsg(IntPtr hMidiOut, uint dwMsg);
 
-        public static (bool,string,string,IntPtr?,MidiOutCaps?) Setup()
+        public static (bool,string,string,IntPtr?,MidiOutCaps?) Setup(int? device)
         {
+            int id = 0;
             int devices = midiOutGetNumDevs();
-            if(devices == 0)
-            {
-                return (false,"None","No WinMM devices were found!",null,null);
-            } else if(devices == 1){
-                MidiOutCaps myCaps = new MidiOutCaps();
-                midiOutGetDevCaps(0, ref myCaps, (UInt32)Marshal.SizeOf(myCaps));
-                IntPtr handle;
-                midiOutOpen(out handle, (uint)0, (IntPtr)0, (IntPtr)0, (uint)0);
-                return (true,myCaps.szPname,"WinMM initialized!",handle,myCaps);
-            } else {
-                Console.WriteLine("WinMM Devices:");
-                for(int i = 0; i < devices; i++)
+            if(device == null){
+                if(devices == 0)
                 {
-                    MidiOutCaps caps = new MidiOutCaps();
-                    midiOutGetDevCaps(i, ref caps, (UInt32)Marshal.SizeOf(caps));
-                    Console.WriteLine((i+1)+": "+caps.szPname);
-                }
-                int id = 0;
-                while(true){
-                    Console.Write("Pick device ID: ");
-                    string? temp = Console.ReadLine();
-                    if(temp == null){Console.WriteLine("\nInvalid input of null");}else{
-                        if (int.TryParse(temp, System.Globalization.NumberStyles.HexNumber, null, out int i))
-                        {
-                            if(i > 0){
-                                id = i-1;
-                                break;
+                    return (false,"None","No WinMM devices were found!",null,null);
+                } else if(devices == 1){
+                    MidiOutCaps myCaps = new MidiOutCaps();
+                    midiOutGetDevCaps(0, ref myCaps, (UInt32)Marshal.SizeOf(myCaps));
+                    IntPtr handle;
+                    midiOutOpen(out handle, (uint)0, (IntPtr)0, (IntPtr)0, (uint)0);
+                    return (true,myCaps.szPname,"WinMM initialized!",handle,myCaps);
+                } else {
+                    Console.WriteLine("WinMM Devices:");
+                    for(int i = 0; i < devices; i++)
+                    {
+                        MidiOutCaps caps = new MidiOutCaps();
+                        midiOutGetDevCaps(i, ref caps, (UInt32)Marshal.SizeOf(caps));
+                        Console.WriteLine((i+1)+": "+caps.szPname);
+                    }
+                    while(true){
+                        Console.Write("Pick device ID: ");
+                        string? temp = Console.ReadLine();
+                        if(temp == null){Console.WriteLine("\nInvalid input of null");}else{
+                            if (int.TryParse(temp, System.Globalization.NumberStyles.HexNumber, null, out int i))
+                            {
+                                if(i > 0){
+                                    id = i-1;
+                                    break;
+                                } else {
+                                    Console.WriteLine("\nCould not parse input");
+                                }
                             } else {
                                 Console.WriteLine("\nCould not parse input");
                             }
-                        } else {
-                            Console.WriteLine("\nCould not parse input");
                         }
                     }
                 }
+            } else {
+                id = (int)device;
+                if(id >= devices || id < 0){
+                    throw new Exception("Selected WinMM device out of range");
+                }
+            }
+            {
                 MidiOutCaps caps1 = new MidiOutCaps();
                 midiOutGetDevCaps(id, ref caps1, (UInt32)Marshal.SizeOf(caps1));
                 IntPtr handle;
