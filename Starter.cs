d121b1968c3b33dbbd1332cd241dd65b0daf4a5c
Emil <50961259+EmK530@users.noreply.github.com>
2022-10-30 02:14:34 +0200

    Update Starter.cs

--- Starter.cs
+++ Starter.cs
@@ -3,6 +3,7 @@ namespace SharpMIDI
     internal class Starter
     {
         public static Form1 form = new Form1();
+        public static bool midiLoaded = false;
 
         [STAThread]
         static void Main()
@@ -12,13 +13,15 @@ namespace SharpMIDI
             Application.Run(form);
         }
 
-        public static async Task SubmitMIDIPath(string str)
+        public static void SubmitMIDIPath(string str)
         {
+            Console.WriteLine("Loading MIDI file: " + str);
+            midiLoaded = true;
             form.label1.Text = "Selected MIDI: " + Path.GetFileName(str);
             form.label2.Text = "Status: Loading";
             form.label1.Update();
             form.label2.Update();
-            await MIDILoader.LoadPath(str,(int)form.numericUpDown1.Value);
+            MIDILoader.LoadPath(str,(int)form.numericUpDown1.Value,(uint)form.numericUpDown2.Value,(int)form.numericUpDown3.Value);
             return;
         }
     }
