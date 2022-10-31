55f0200c4e89d96c26ec57f2f8240f6db8e81d4b
Emil <50961259+EmK530@users.noreply.github.com>
2022-10-31 11:37:10 +0100

    Add files via upload

---
+++ SharpMIDI.Playback/MIDIClock.cs
@@ -0,0 +1,62 @@
+using System.Diagnostics;
+
+namespace SharpMIDI
+{
+    class MIDIClock
+    {
+        static double time = 0f;
+        static double bpm = 120f;
+        public static double ppq = 0;
+        static double ticklen;
+        static Stopwatch test = new Stopwatch();
+        static double last = 0;
+        public static bool throttle = true;
+        public static void Start()
+        {
+            test.Start();
+            ticklen = (1 / (double)ppq) * (60 / bpm);
+        }
+
+        static double GetElapsed()
+        {
+            double temp = ((double)test.ElapsedTicks / TimeSpan.TicksPerSecond);
+            if (throttle)
+            {
+                if (temp-last > 0.0166666d)
+                {
+                    temp = last + 0.0166666d;
+                    last = temp;
+                    return temp;
+                }
+            }
+            last = temp;
+            return temp;
+        }
+
+        public static void SubmitBPM(double p, double b)
+        {
+            double remainder = (time - p);
+            time = p + (GetElapsed() / ticklen);
+            bpm = 60000000 / b;
+            Console.WriteLine("New BPM: " + bpm);
+            ticklen = (1 / (double)ppq) * (60 / bpm);
+            time += remainder;
+            test.Restart();
+        }
+
+        public static double GetTick()
+        {
+            return time + (GetElapsed() / ticklen);
+        }
+
+        public static void Stop()
+        {
+            test.Stop();
+        }
+
+        public static void Resume()
+        {
+            test.Start();
+        }
+    }
+}
