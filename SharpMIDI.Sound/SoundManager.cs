2163804341da0706f6f33e73daefa402c3f31a0e
Emil <50961259+EmK530@users.noreply.github.com>
2022-11-30 00:44:50 +0100

    Replace switch case with variable

--- SharpMIDI.Sound/SoundManager.cs
+++ SharpMIDI.Sound/SoundManager.cs
@@ -1,12 +1,27 @@
+using System.Runtime.CompilerServices;
+
 namespace SharpMIDI
 {
-    class Sound
+    unsafe class Sound
     {
         private static int engine = 0;
         public static long totalEvents = 0;
         static string lastWinMMDevice = "";
         private static IntPtr? handle;
         static System.Diagnostics.Stopwatch watch = System.Diagnostics.Stopwatch.StartNew();
+        public static Func<uint,uint> sendTo;
+        static uint stWinMM(uint ev)
+        {
+            return WinMM.midiOutShortMsg((IntPtr)handle, ev);
+        }
+        static uint stXSynth(uint ev)
+        {
+            return XSynth.SendDirectData(ev);
+        }
+        static uint stKDMAPI(uint ev)
+        {
+            return KDMAPI.SendDirectData(ev);
+        }
         public static bool Init(int synth, string winMMdev)
         {
             Close();
@@ -21,6 +36,7 @@ namespace SharpMIDI
                         if (loaded == 1)
                         {
                             engine = 1;
+                            sendTo = stKDMAPI;
                             return true;
                         }
                         else { return false; }
@@ -36,6 +52,7 @@ namespace SharpMIDI
                     else
                     {
                         engine = 2;
+                        sendTo = stWinMM;
                         handle = result.Item4;
                         lastWinMMDevice = winMMdev;
                         return true;
@@ -49,6 +66,7 @@ namespace SharpMIDI
                         if (loaded == 1)
                         {
                             engine = 3;
+                            sendTo = stXSynth;
                             return true;
                         }
                         else { MessageBox.Show("KDMAPI is not available."); return false; }
@@ -77,19 +95,7 @@ namespace SharpMIDI
         }
         public static void Submit(uint ev)
         {
-            switch(engine){
-                case 3:
-                    XSynth.SendDirectData(ev);
-                    break;
-                case 1:
-                    KDMAPI.SendDirectData(ev);
-                    break;
-                case 2:
-                    if(handle!=null){
-                        WinMM.midiOutShortMsg((IntPtr)handle,ev);
-                    }
-                    break;
-            }
+            sendTo(ev);
             totalEvents++;
         }
         public static void Close(){
