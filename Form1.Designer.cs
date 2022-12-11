02f8f45618e427c1c9dd814c568b4a009dd64dbc
Emil <50961259+EmK530@users.noreply.github.com>
2022-12-11 20:18:22 +0100

    Wrong version number

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -71,7 +71,7 @@
             this.Title.Name = "Title";
             this.Title.Size = new System.Drawing.Size(496, 40);
             this.Title.TabIndex = 0;
-            this.Title.Text = "SharpMIDI v3.1.2";
+            this.Title.Text = "SharpMIDI v3.1.3";
             // 
             // button1
             // 
