936a9b7d9634da51c051c5490e58b6cc7544ded7
Emil <50961259+EmK530@users.noreply.github.com>
2023-01-22 17:56:45 +0100

    Create ArchiveLoader.cs

---
+++ SharpMIDI.MIDI/ArchiveLoader.cs
@@ -0,0 +1,10 @@
+namespace SharpMIDI
+{
+    class ArchiveLoader
+    {
+        public static void LoadPath(string path)
+        {
+            throw new Exception("Archive loading not implemented.");
+        }
+    }
+}
