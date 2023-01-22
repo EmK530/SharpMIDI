802320fdbe5c1ee6813b2d47f99ef20985c6358c
Emil <50961259+EmK530@users.noreply.github.com>
2023-01-22 17:54:05 +0100

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,17 @@
+﻿namespace SharpMIDI
+{
+    public static class Starter
+    {
+        public static string level = "Beta 1";
+        public static string major = "4";
+        public static string minor = "0";
+        public static string micro = "0";
+        public static string name = "SharpMIDI v"+major+"."+minor+"."+micro+" "+level;
+        [STAThread]
+        public static void Main()
+        {
+            Window wnd = new Window();
+            wnd.Run();
+        }
+    }
+}
