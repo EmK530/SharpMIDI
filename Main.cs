dfaf7c4a14dc271a332d62b61a3bf4a19dcf3b89
Emil <50961259+EmK530@users.noreply.github.com>
2023-02-09 19:48:16 +0100

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -1,12 +1,12 @@
-﻿namespace SharpMIDI
+namespace SharpMIDI
 {
     public static class Starter
     {
-        public static string level = "Beta 1";
+        public static string level = " Beta 3";
         public static string major = "4";
         public static string minor = "0";
         public static string micro = "0";
-        public static string name = "SharpMIDI v"+major+"."+minor+"."+micro+" "+level;
+        public static string name = "SharpMIDI v"+major+"."+minor+"."+micro+level;
         [STAThread]
         public static void Main()
         {
