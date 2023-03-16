f8ee9a393c47da3d3e7c71e6602865a6a20b0010
Emil <50961259+EmK530@users.noreply.github.com>
2023-03-16 23:26:39 +0100

    Change SynthEvent val to Int24

--- SharpMIDI.MIDI/DataStorage.cs
+++ SharpMIDI.MIDI/DataStorage.cs
@@ -1,3 +1,5 @@
+using System.Runtime.InteropServices;
+
 namespace SharpMIDI
 {
     public struct Tempo
@@ -8,7 +10,48 @@ namespace SharpMIDI
     public struct SynthEvent
     {
         public float pos;
-        public uint val;
+        public Int24 val;
+    }
+    [StructLayout(LayoutKind.Explicit)]
+    public struct Int24
+    {
+        [FieldOffset(0)]
+        private byte b0;
+        [FieldOffset(1)]
+        private byte b1;
+        [FieldOffset(2)]
+        private byte b2;
+
+        [FieldOffset(0)]
+        private int value;
+
+        public Int24(int value)
+        {
+            this.b0 = this.b1 = this.b2 = 0;
+            this.value = value;
+        }
+
+        public int Value
+        {
+            get
+            {
+                return this.value;
+            }
+            set
+            {
+                this.value = value;
+            }
+        }
+
+        public static implicit operator Int24(int value)
+        {
+            return new Int24(value);
+        }
+
+        public static implicit operator int(Int24 value)
+        {
+            return value.value;
+        }
     }
     class MIDIData
     {
